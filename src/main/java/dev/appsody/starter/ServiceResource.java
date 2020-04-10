48b29cfa227776eb40e245de7821e896ec301e46
Denilson Nastacio <dnastaci@us.ibm.com>
2020-04-10 12:19:28 -0400

    first commit

---
+++ src/main/java/dev/appsody/starter/ServiceResource.java
@@ -0,0 +1,30 @@
+package dev.appsody.starter;
+
+import javax.inject.Inject;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.xml.ws.Response;
+
+import io.opentracing.Tracer;
+
+@Path("/service")
+public class ServiceResource {
+
+    @Inject
+    Tracer tracer;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response completeOrder(JsonObject orderPayload, 
+                                  @Context HttpHeaders httpHeaders) {
+        JsonObject response = Json.createObjectBuilder().add("status", "completed")
+                .add("order", orderPayload.getString("order")).build();
+        return Response.ok(response).build();
+    }
+}
